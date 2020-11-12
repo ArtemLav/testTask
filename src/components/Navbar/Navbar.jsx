d4b5cbd07c8aa3e3ddfeb7a5d8eb85f19de6bb16
Artem Lavreniuk <artemlavrenyuk@gmail.com>
2020-11-12 15:18:35 +0200

    layout updated, mobile nav added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,49 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
 import './Navbar.scss';
 
-export const Navbar = () => (
-  <nav className="header__nav nav">
-    <ul className="nav__list">
-      <li className="nav__item">
-        <a
-          href="#registration-form"
-          className="nav__link text-decoration-none"
-        >
-          About me
-        </a>
-      </li>
-      <li className="nav__item">
-        <a
-          href="#registration-form"
-          className="nav__link text-decoration-none"
-        >
-          Relationships
-        </a>
-      </li>
-      <li className="nav__item">
-        <a
-          href="#registration-form"
-          className="nav__link text-decoration-none"
-        >
-          Requirements
-        </a>
-      </li>
-      <li className="nav__item">
-        <a
-          href="#registration-form"
-          className="nav__link text-decoration-none"
-        >
-          Users
-        </a>
-      </li>
-      <li className="nav__item">
-        <a
-          href="#registration-form"
-          className="nav__link text-decoration-none"
-        >
-          Sign up
-        </a>
-      </li>
-    </ul>
-  </nav>
-);
+export const Navbar = ({ currentNav }) => {
+  const [activeLink, setActiveLink] = useState('');
+
+  const setAsActive = (event) => {
+    setActiveLink(event.target.textContent);
+  };
+
+  return (
+    <nav className="header__nav nav">
+      <ul className="nav__list">
+        {currentNav.map(link => (
+          <li className="nav__item" key={link.id}>
+            <a
+              href="#registration-form"
+              className={classnames('nav__link text-decoration-none', {
+                nav__link_active: activeLink === link.name,
+              })}
+              onClick={setAsActive}
+            >
+              {link.name}
+            </a>
+            {(currentNav.length > 5
+              && (link.id % 5 === 0)
+              && link.id !== currentNav.length) && (
+              <hr className="nav__link-separator" />
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  currentNav: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+  })).isRequired,
+};
